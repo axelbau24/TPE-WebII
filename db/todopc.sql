e22d3884633bac7df0832ffe93de0c1be79084af
Axel Bautista <axelbau24@gmail.com>
2016-09-23 23:29:31 -0300

    Actualizada estructura base de datos

--- db/todopc.sql
+++ db/todopc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-09-2016 a las 22:31:50
+-- Tiempo de generación: 24-09-2016 a las 04:28:20
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.21
 
@@ -48,7 +48,7 @@ INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
 
 CREATE TABLE `componente` (
   `id_componente` int(11) NOT NULL,
-  `nombre` varchar(20) NOT NULL,
+  `nombre` varchar(35) NOT NULL,
   `destacado` tinyint(1) NOT NULL,
   `fk_id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -58,12 +58,35 @@ CREATE TABLE `componente` (
 --
 
 INSERT INTO `componente` (`id_componente`, `nombre`, `destacado`, `fk_id_categoria`) VALUES
-(1, 'ASUS X99-DELUXE II', 0, 0),
-(2, 'ASUS Z170-A', 0, 0),
-(3, 'Samsung 850 EVO', 0, 0),
-(4, 'AMD Radeon RX 480', 0, 0),
-(5, 'Nvidia GeForce GTX 1', 0, 0),
-(6, 'MSI Z170A M6', 1, 0);
+(1, 'ASUS X99-DELUXE II', 1, 1),
+(2, 'ASUS Z170-A', 0, 1),
+(3, 'Samsung 850 EVO', 1, 3),
+(4, 'AMD Radeon RX 480', 0, 2),
+(5, 'Nvidia GeForce GTX 1080', 0, 2),
+(6, 'MSI Z170A M6', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
+--
+
+CREATE TABLE `imagen` (
+  `id_imagen` int(11) NOT NULL,
+  `ruta` varchar(120) NOT NULL,
+  `fk_id_componente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `imagen`
+--
+
+INSERT INTO `imagen` (`id_imagen`, `ruta`, `fk_id_componente`) VALUES
+(1, 'images/57e5e19615eba_1_1.png', 1),
+(2, 'images/57e5e1af2d082_2_1.png', 1),
+(3, 'images/57e5e1bb0e48a_3_1.png', 1),
+(4, 'images/57e5e38ad00f6_4_5.png', 5),
+(5, 'images/57e5e3461561c_5_5.png', 5);
 
 --
 -- Índices para tablas volcadas
@@ -83,6 +106,12 @@ ALTER TABLE `componente`
   ADD KEY `fk_id_categoria` (`fk_id_categoria`);
 
 --
+-- Indices de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD PRIMARY KEY (`id_imagen`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -96,6 +125,11 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `componente`
   MODIFY `id_componente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  MODIFY `id_imagen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
